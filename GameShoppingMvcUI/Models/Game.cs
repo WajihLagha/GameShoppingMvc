df53ef6936c202f82226ffc6f59a7f1fcb485a69
CodeFather10 <133059999+CodeFather10@users.noreply.github.com>
2025-12-04 20:33:19 +0100

    Add project files.

---
+++ GameShoppingMvcUI/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameShoppingMvcUI.Models
+{
+    [Table("Game")]
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string? GameName { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public Genre? Genre { get; set; }
+
+    }
+}
---
+++ GameShoppingMvcUI/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameShoppingMvcUI.Models
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string? GenreName { get; set; }
+        public List<Game>? Games { get; set; }
+    }
+}
