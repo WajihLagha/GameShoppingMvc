30997d7d0d5ec0ab8c8bf93f6808121d3595c1ef
CodeFather10 <133059999+CodeFather10@users.noreply.github.com>
2025-12-13 11:37:58 +0100

    Edit the login register UI

--- GameShoppingMvcUI/Migrations/20251213100141_update-models.Designer.cs
+++ GameShoppingMvcUI/Migrations/20251213100141_update-models.Designer.cs
@@ -4,6 +4,7 @@ using GameShoppingMvcUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameShoppingMvcUI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251213100141_update-models")]
+    partial class updatemodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GameShoppingMvcUI/Migrations/20251213100141_update-models.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameShoppingMvcUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Genre",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ShoppingCart",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "ShoppingCart");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Genre",
+                newName: "id");
+        }
+    }
+}
